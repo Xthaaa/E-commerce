5ccc86f8f8c07e0aec7cf6e19d2b5dbd97a79cb7
Yash Porwal <yashporwal@Yashs-MacBook-Air.local>
2025-06-22 10:26:51 +0530

    yash

--- lib/Home screen/home_page.dart
+++ lib/Home screen/home_page.dart
@@ -12,27 +12,30 @@ class HomePage extends StatelessWidget {
       body: Center(
         child: SizedBox(
           width: 376,
-          child: Column(
-            children:  [
-              Components3(),
-              SizedBox(height: 10), // spacing before bottom bar if needed
-            ],
+          child: SingleChildScrollView(
+            // <-- Added this
+            child: Column(
+              children: [
+                Components3(),
+                SizedBox(height: 10), // spacing before bottom bar if needed
+              ],
+            ),
           ),
         ),
       ),
       bottomNavigationBar: SizedBox(
-          width: 376,
-          child: ClipRRect(
-            borderRadius: BorderRadius.only(
-              topLeft: Radius.circular(20),
-              topRight: Radius.circular(20),
-            ),
-            child:  BottomAppBar(
-              color: Color(0xffFFFFFF),
-              child: Components2(),
-            ),
+        width: 376,
+        child: ClipRRect(
+          borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(20),
+            topRight: Radius.circular(20),
+          ),
+          child: BottomAppBar(
+            color: Color(0xffFFFFFF),
+            child: Components2(),
           ),
         ),
-      );
+      ),
+    );
   }
 }
